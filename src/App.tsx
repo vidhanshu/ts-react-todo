6a52aae132f89bc46b7a739805c36c4040f01e18
vidhanshu <vidhanshu7218555039@gmail.com>
2022-07-15 02:40:24 +0530

    try2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { createContext, useState } from "react";
 import Form from "./components/form/Form";
 import List from "./components/list/List";
-import "./app.css";
 
 interface Task {
   id: number;
